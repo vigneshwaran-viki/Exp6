58070d8b9a9200ec8b4eb1aee15a40d098606881
binarysearch <itcfav@gmail.com>
2024-03-17 13:00:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/exp6/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.exp6;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+public class MainActivity extends AppCompatActivity
+{
+    ImageView img;
+    Button bt1,bt2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt1 = (Button)findViewById(R.id.button);
+        bt2= (Button) findViewById(R.id.button2);
+        img = (ImageView)findViewById(R.id.imageView);
+        bt1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                new Thread(new Runnable(){
+                    @Override
+                    public void run(){
+                        img.post(new Runnable(){
+                            @Override
+                            public void run()
+                            {
+                                img.setImageResource(R.drawable.india1);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v){
+                new Thread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        img.post(new Runnable()
+                        {
+                            @Override
+                            public void run()
+                            {
+                                img.setImageResource(R.drawable.india2);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });}}
